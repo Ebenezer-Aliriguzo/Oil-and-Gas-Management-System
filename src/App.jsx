83434e65fbc818da81170a7ce0e53b86eb57de31
Ebenezer Aliriguzo <139380746+EbenyICT@users.noreply.github.com>
2025-04-23 05:43:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route
-          path="/termsofserviceandprivacypolicy"
+          path="/termsOfserviceAndprivacypolicy"
           element={<TermsOfServiceAndPrivacyPolicy />}
         />
       </Routes>
