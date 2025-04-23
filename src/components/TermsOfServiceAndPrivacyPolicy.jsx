e55591dd08767323ef43901ec8347bc64842466c
Ebenezer Aliriguzo <139380746+EbenyICT@users.noreply.github.com>
2025-04-23 05:23:06 +0100

    Update TermsOfServiceAndPrivacyPolicy.jsx

--- src/components/TermsOfServiceAndPrivacyPolicy.jsx
+++ src/components/TermsOfServiceAndPrivacyPolicy.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './TermsofServiceandPrivacyPolicy.css';
+import './TermsOfServiceAndPrivacyPolic.css';
 
 const TermsofServiceandPrivacyPolicy = () => {
   return (
